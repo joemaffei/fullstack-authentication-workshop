a601f5db675cc98846f23f9a75dc5d9026064061
Joe Maffei <jmaffei@felux.com>
2022-11-14 10:20:03 -0600

    format server.js

--- server.js
+++ server.js
@@ -33,10 +33,9 @@ app.use(express.urlencoded({
 
 
 app.get("*", (req, res) => {
-    res.sendFile(__dirname + "public/index.html"); 
+    res.sendFile(__dirname + "public/index.html");
 });
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 });
-
