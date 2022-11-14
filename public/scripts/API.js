0d631e832c9d4d3ff785fa75b3ae3cad15e21a92
Joe Maffei <jmaffei@felux.com>
2022-11-14 10:37:38 -0600

    endpoints for WebAuthn

--- public/scripts/API.js
+++ public/scripts/API.js
@@ -13,6 +13,26 @@ const API = {
     register: async (user) => {
         return await API.makePostRequest(API.endpoint + "register", user);
     },
+    webAuthn: {
+        loginOptions: async (email) => {
+            return await API.makePostRequest(API.endpoint + "webauth-login-options", { email });
+        },
+        loginVerification: async (email, data) => {
+            return await API.makePostRequest(API.endpoint + "webauth-login-verification", {
+                email,
+                data
+            });
+        },
+        registrationOptions: async () => {
+            return await API.makePostRequest(API.endpoint + "webauth-registration-options", Auth.account);
+        },
+        registrationVerification: async (data) => {
+            return await API.makePostRequest(API.endpoint + "webauth-registration-verification", {
+                user: Auth.account,
+                data
+            });
+        }
+    },
     makePostRequest: async (url, data) => {
         const response = await fetch(url, {
             method: "POST",
