105f9a413e75f9d34bf771da3df42497269ad10b
Joe Maffei <jmaffei@felux.com>
2022-11-14 10:23:45 -0600

    add login and register UI methods to Auth.js

--- public/scripts/Auth.js
+++ public/scripts/Auth.js
@@ -4,6 +4,30 @@ import Router from "./Router.js";
 const Auth = {
     isLoggedIn: false,
     account: null,
+    login: async (event) => {
+        if (event) event.preventDefault();
+        const user = {
+            email: document.getElementById("login_email").value,
+            password: document.getElementById("login_password").value
+
+        };
+        const response = await API.login(user);
+        Auth.postLogin(response, {
+            ...user,
+            name: response.name
+        });
+
+    },
+    register: async  (event) => {
+        event.preventDefault();
+        const user = {
+            name: document.getElementById("register_name").value,
+            email: document.getElementById("register_email").value,
+            password: document.getElementById("register_password").value
+        }
+        const response = await API.register(user);
+        Auth.postLogin(response, user);
+    },
     updateStatus() {
         if (Auth.isLoggedIn && Auth.account) {
             document.querySelectorAll(".logged_out").forEach(
